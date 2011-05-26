8878332ef18f7850e0bced93578a96a1b840412e
Jonas Eriksson <jonas@programenta.se>
2011-05-26 21:37:50 +0200

    Cleaned up form creation tests and templates

--- DynaForms/DynaForms_Test/FormCreation.cs
+++ DynaForms/DynaForms_Test/FormCreation.cs
@@ -10,6 +10,8 @@ namespace DynaForms_Test
     [TestClass]
     public class FormCreation
     {
+        static string defaultOutputPath = @"C:\Users\joeriks\Documents\GitHub\DynaForms\DynaForms\DynaForms_Test\resources\";
+
         [TestMethod]
         public void FormCreation_NoErrors()
         {
@@ -17,107 +19,121 @@ namespace DynaForms_Test
             var x = new DynaForms.DynaForm("formname", project);
             Assert.AreEqual(x.Name,"formname");
         }
-
-        [TestMethod]
-        [DeploymentItem("resources//CustomTemplate.htm")]        
-        public void FormCreation_CustomTemplate()
-        {
-            var project = new Project();
-
-            var customTemplate = "<input type='text' id='{fieldName}' name='{fieldName}' value='{value}'/>{errorMessage}\n";
-
-            var f = new DynaForms.DynaForm("formname", project);
-            f.AddFormField("Name", template: customTemplate);
-            f.AddFormField("Address", template: customTemplate);
-
-            System.IO.File.WriteAllText(@"C:\Users\jonas\DynaForms\DynaForms\DynaForms_Test\resources\CustomTemplate.htm", f.Html().ToString());
-            var expectedResult = System.IO.File.ReadAllText("CustomTemplate.htm");
-            Assert.AreEqual(expectedResult, f.Html().ToString());
-        }
         
         [TestMethod]
-        [DeploymentItem("resources//WithHtml.htm")]
-        public void FormCreation_WithHtml()
+        [DeploymentItem("resources//CustomHtml.htm")]
+        public void FormCreation_CustomHtml()
         {
+            string formName = "CustomHtml";
+            string fileName = formName + ".htm";
+
             var project = new Project();
-            var f = new DynaForms.DynaForm("formname", project);
+            var form = new DynaForms.DynaForm(formName, project);
 
-            f.AddHtml("<fieldset>\n");
-            f.AddHtml("  <legend>Fieldset 1</legend>\n"); 
-            f.AddFormField("Name");
-            f.AddFormField("Address");
-            f.AddHtml("</fieldset>\n");
+            form.AddHtml("<fieldset>\n");
+            form.AddHtml("  <legend>Fieldset 1</legend>\n"); 
+            form.AddFormField("Name");
+            form.AddFormField("Address");
+            form.AddHtml("</fieldset>\n");
 
-            f.AddHtml("<fieldset>\n");
-            f.AddHtml("  <legend>Fieldset 2</legend>\n"); 
-            f.AddFormField("Phone 1");
-            f.AddFormField("Phone 2");
-            f.AddHtml("</fieldset>\n");
+            form.AddHtml("<fieldset>\n");
+            form.AddHtml("  <legend>Fieldset 2</legend>\n"); 
+            form.AddFormField("Phone 1");
+            form.AddFormField("Phone 2");
+            form.AddHtml("</fieldset>\n");
 
-            f.AddHtml("<fieldset>\n");
-            f.AddFormField("Submit", type: InputType.submit);
-            f.AddHtml("</fieldset>\n");
+            form.AddHtml("<fieldset>\n");
+            form.AddFormField("Submit", type: InputType.submit);
+            form.AddHtml("</fieldset>\n");
 
-            System.IO.File.WriteAllText(@"C:\Users\jonas\DynaForms\DynaForms\DynaForms_Test\resources\WithHtml.htm", f.Html().ToString());
-            var expectedResult = System.IO.File.ReadAllText("WithHtml.htm");
-            Assert.AreEqual(expectedResult, f.Html().ToString());
+            System.IO.File.WriteAllText(defaultOutputPath + fileName, form.Html().ToString());
+            var expectedResult = System.IO.File.ReadAllText(fileName);
+            Assert.AreEqual(expectedResult, form.Html().ToString());
 
         }
         
         [TestMethod]
-        [DeploymentItem("resources//form1.htm")]
-        public void FormCreationHtml_CompareToFile_Form1()
+        [DeploymentItem("resources//Simple.htm")]
+        public void FormCreation_Simple()
         {
-            var f = new DynaForms.DynaForm("myform");
+            string formName = "Simple";
+            string fileName = formName + ".htm";
+
+            var f = new DynaForms.DynaForm(formName);
             f.AddFormField("name").AddFormField("value");
 
-            System.IO.File.WriteAllText(@"C:\Users\jonas\DynaForms\DynaForms\DynaForms_Test\resources\form1.htm", f.Html().ToString());
-            var expectedResult = System.IO.File.ReadAllText("form1.htm");            
+            System.IO.File.WriteAllText(defaultOutputPath + fileName, f.Html().ToString());
+            var expectedResult = System.IO.File.ReadAllText(fileName);            
             Assert.AreEqual(expectedResult, f.Html().ToString());
         }
 
         [TestMethod]
-        [DeploymentItem("resources//form4.htm")]
-        public void FormCreationHtml_CompareToFile_Form2()
+        [DeploymentItem("resources//WithDropDown.htm")]
+        public void FormCreation_WithDropDown()
         {
-            var f = new DynaForms.DynaForm("form4");
+            string formName = "WithDropDown";
+            string fileName = formName + ".htm";
+
+            var form = new DynaForms.DynaForm(formName);
             var dropDownValues = new Dictionary<string, string>();
             dropDownValues.Add("1", "one");
             dropDownValues.Add("2", "two");
             dropDownValues.Add("3", "three");
 
-            f.AddFormField("name")
+            form.AddFormField("name")
              .AddFormField("check",type:InputType.checkbox)
              .AddFormField("value",type:InputType.textarea)
              .AddFormField("dropdown", type:InputType.select, dropDownValues: dropDownValues)
              .AddFormField("submit", type:InputType.submit);
 
-            System.IO.File.WriteAllText(@"C:\Users\jonas\DynaForms\DynaForms\DynaForms_Test\resources\form4.htm", f.Html().ToString());
-            var expectedResult = System.IO.File.ReadAllText("form4.htm");
-            Assert.AreEqual(expectedResult, f.Html().ToString());
+            System.IO.File.WriteAllText(defaultOutputPath + fileName, form.Html().ToString());
+            var expectedResult = System.IO.File.ReadAllText(fileName);
+            Assert.AreEqual(expectedResult, form.Html().ToString());
         }
 
         [TestMethod]
-        [DeploymentItem("resources//form5.htm")]
-        public void FormCreationHtml_CompareToFile_Form5()
+        [DeploymentItem("resources//WithDataObject.htm")]
+        public void FormCreation_WithDataObject()
         {
+            string formName = "WithDataObject";
+            string fileName = formName + ".htm";
+
             var p = new Project();
             p.Check = true;
             p.Name = "existing";
             p.PrintedDateTime = null;
             p.CreationDate = new DateTime(2000, 10, 20);
 
-            var f = new DynaForms.DynaForm("myform", p);
-            f.AddFormField("Name")
+            var form = new DynaForms.DynaForm(formName, p);
+            form.AddFormField("Name")
              .AddFormField("Check",type:InputType.checkbox)
              .AddFormField("PrintedDateTime")
              .AddFormField("CreationDate");
 
-            System.IO.File.WriteAllText(@"C:\Users\jonas\DynaForms\DynaForms\DynaForms_Test\resources\form5.htm", f.Html().ToString());
-            var expectedResult = System.IO.File.ReadAllText("form5.htm");
-            Assert.AreEqual(expectedResult, f.Html().ToString());
+            System.IO.File.WriteAllText(defaultOutputPath + fileName, form.Html().ToString());
+            var expectedResult = System.IO.File.ReadAllText(fileName);
+            Assert.AreEqual(expectedResult, form.Html().ToString());
         }
 
+        [TestMethod]
+        [DeploymentItem("resources//FormCreation_CustomBasicTemplate.htm")]
+        public void FormCreation_CustomBasicTemplate()
+        {
+            var inputText = "{labelText} : <input type='text' {idName} />{errorMessage}<br/>";
+
+            var testForm = new DynaForm("formname", autoAddSubmit: false)
+                   .AddFormField("Name", template: inputText)
+                   .AddFormField("Phonenumber", "Phone number", template: inputText)
+                   .AddHtml("<input type='submit'/>");
+
+            // Uncomment the following row to re-create output html - then look at it manually and open it in a browser
+            System.IO.File.WriteAllText(defaultOutputPath + "FormCreation_CustomBasicTemplate.htm", testForm.Html().ToString());
+            // After you recreated it you need to include the file in the project and also mark it (properties on the file) for "Copy to output"
+
+            var expectedResult = System.IO.File.ReadAllText("FormCreation_CustomBasicTemplate.htm");
+            Assert.AreEqual(expectedResult, testForm.Html().ToString());
+
+        }
 
     }
 }
