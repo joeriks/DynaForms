b6b9ec323502119c5d82a0390bed6752d994d668
Jonas Eriksson <jonas@programenta.se>
2011-05-26 21:13:43 +0200

    added gitignore

---
+++ DynaForms/DynaForms_Test/FormCreationWithHtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynaForms;
+
+namespace DynamicForms_Test
+{
+    [TestClass]
+    public class FormCreationWithHtml
+    {
+        [TestMethod]
+        public void CreateFormWithInputText()
+        {
+            var inputText = "{labelText} : <input type='text' {idName} />{errorMessage}<br/>";
+
+            var testForm = new DynaForm("formname", autoAddSubmit: false)
+                   .AddFormField("Name", template: inputText)
+                   .AddFormField("Phonenumber", "Phone number", template: inputText)
+                   .AddHtml("<input type='submit'/>");
+
+            var result = testForm.Html();
+            Assert.AreEqual("", result);
+
+        }
+    }
+}
