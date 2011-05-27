e35140a144f017ec08ccff1e30e892306a41d54b
Jonas Eriksson <jonas@programenta.se>
2011-05-27 14:14:20 +0200

    Added another sample WebPages (PetaPoco), one page CRUD

---
+++ Sample_PetaPoco/App_Code/Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Company
+/// </summary>
+
+[PetaPoco.PrimaryKey("id")]
+public class Company
+{
+	public Company()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}   
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDateTime { get; set; }
+    public string Note { get; set; }
+    //public bool Active { get; set; }
+
+}
