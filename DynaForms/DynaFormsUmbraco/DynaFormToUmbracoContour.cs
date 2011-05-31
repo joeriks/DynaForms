5fae5a6428759f38d084dd686ef50d1861feb25b
Jonas Eriksson <jonas@programenta.se>
2011-05-31 16:29:11 +0200

    Added DynaFormsToUmbracoContour.cs thats adds a extensionmethod to DynaForm. Usage: myDynaForm.SaveToContour("form guid",ipAddress, umbracoNodeId) returns successfully added record id.

---
+++ DynaForms/DynaFormsUmbraco/DynaFormToUmbracoContour.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Services;
+using Umbraco.Forms.Data.Storage;
+using DynaForms;
+
+namespace DynaFormsExtensionMethods
+{
+    public static class DynaFormToUmbracoContour
+    {
+        public static string SaveToContour(this DynaForm dynaform, string formGuid, string userIpAddress = "", int umbracoPageId = 0)
+        {
+            var retval = "";
+
+            using (var recordStorage = new RecordStorage())
+            using (var formStorage = new FormStorage())
+            {
+                var form = formStorage.GetForm(new Guid(formGuid));
+
+                using (var recordService = new RecordService(form))
+                {
+                    recordService.Open();
+
+                    var record = recordService.Record;
+                    record.IP = userIpAddress;
+                    record.UmbracoPageId = umbracoPageId;
+                    recordStorage.InsertRecord(record, form);
+
+                    foreach (var field in recordService.Form.AllFields)
+                    {
+
+                        string currentFieldValue = "";
+                        string contourFieldName = field.Caption.TrimStart('#');
+
+                        if (dynaform.ModelDictionary.ContainsKey(contourFieldName))
+                        {
+                            currentFieldValue = dynaform.ModelDictionary[contourFieldName].ToString();
+                        }
+
+                        var key = Guid.NewGuid();
+                        var recordField = new RecordField
+                        {
+                            Field = field,
+                            Key = key,
+                            Record = record.Id,
+                            Values = new List<object> { currentFieldValue }
+                        };
+
+                        using (var recordFieldStorage = new RecordFieldStorage())
+                            recordFieldStorage.InsertRecordField(recordField);
+
+                        record.RecordFields.Add(key, recordField);
+
+                        retval = record.Id.ToString();
+                    }
+                    recordService.Submit();
+                    recordService.SaveFormToRecord();
+
+                }
+            }
+            return retval;
+
+        }
+    }
+}
